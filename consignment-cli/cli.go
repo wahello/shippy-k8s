8d80eb0c4c58cb8fe4d001e321c1f8647ff8d99e
Chris Gault <ccgault@gmail.com>
2018-12-25 18:54:05 -0600

    init

---
+++ consignment-cli/cli.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	microclient "github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/cmd"
+	"golang.org/x/net/context"
+
+	pb "shippy/consignment-service/proto/consignment"
+)
+
+const (
+	defaultFilename string = "consignment.json"
+)
+
+func parseFile(file string) (*pb.Consignment, error) {
+	var consignment *pb.Consignment
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(data, &consignment)
+	return consignment, err
+}
+
+func main() {
+	cmd.Init()
+	client := pb.NewShippingService("go.micro.srv.consignment", microclient.DefaultClient)
+
+	file := defaultFilename
+	if len(os.Args) > 1 {
+		file = os.Args[1]
+	}
+
+	consignment, err := parseFile(file)
+	if err != nil {
+		log.Fatalf("could not parse file: %v", err)
+	}
+
+	r, err := client.CreateConsignment(context.Background(), consignment)
+	if err != nil {
+		log.Fatalf("could not create: %v", err)
+	}
+	log.Printf("created: %t", r.Created)
+
+	getAll, err := client.GetConsignments(context.Background(), &pb.GetRequest{})
+	if err != nil {
+		log.Fatalf("could not list consignments: %v", err)
+	}
+	for _, v := range getAll.Consignments {
+		log.Println(v)
+	}
+}
---
+++ consignment-service/main.go
@@ -0,0 +1,66 @@
+// Package main provides ...
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "shippy/consignment-service/proto/consignment"
+
+	micro "github.com/micro/go-micro"
+)
+
+const (
+	port string = ":50051"
+)
+
+type Repository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
+}
+
+type repository struct {
+	consignments []*pb.Consignment
+}
+
+func (r *repository) Create(c *pb.Consignment) (*pb.Consignment, error) {
+	u := append(r.consignments, c)
+	r.consignments = u
+	return c, nil
+}
+
+func (r *repository) GetAll() []*pb.Consignment {
+	return r.consignments
+}
+
+type service struct {
+	repo Repository
+}
+
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+	c, err := s.repo.Create(req)
+	if err != nil {
+		return err
+	}
+	res.Created = true
+	res.Consignment = c
+	return nil
+}
+
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
+	res.Consignments = s.repo.GetAll()
+	return nil
+}
+
+func main() {
+	repo := &repository{}
+	srv := micro.NewService(
+		micro.Name("go.micro.srv.consignment"),
+		micro.Version("latest"),
+	)
+	srv.Init()
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+	if err := srv.Run(); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
